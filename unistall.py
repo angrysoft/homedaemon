54c06610c1a72abf7c0a765a2c3f544f7916fe31
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2018-06-26 20:38:50 +0200

    uninstall

--- unistall.py
+++ unistall.py
@@ -4,7 +4,9 @@ import os
 
 if os.path.exists('files.txt'):
     with open('files.txt') as ff:
-        for f in ff.readline():
+        for f in ff.readlines():
+            f = f.strip()
             if os.path.exists(f):
                 print('Remove {}'.format(f))
-                //os.unlink(f)
+                os.unlink(f)
+
