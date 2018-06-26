fba2de5455d2b54eb8abde625f2dba5029886b4e
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2018-06-26 17:06:16 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,25 +1,38 @@
 from distutils.core import setup
 from glob import glob
+import os
+
+
+def get_files(name):
+    for dirname, dirnames, filenames in os.walk(name):
+        if dirname == name:
+            return [os.path.join(dirname, f) for f in filenames]
+        else:
+            continue
+
 
 confFile = glob('files/*.json')
-wwwStatic = glob('src/static/*css') + glob('src/static/*.dart') + glob('src/static/*.dart.js')
-wwwTemp = glob('src/templates/*.html')
-
+wwwStatic = get_files('src/static')
+wwwStaticRf = get_files('src/static/rfpilot')
+wwwStaticTv = get_files('src/static/tvpilot')
+wwwTemp = get_files('src/templates')
 
 setup(name='SmartHouse',
-      version='0.4',
+      version='0.5',
       description='House Automatization',
       url='https://bitbucket.org/angrysoft/Smarthouse',
       author='Sebastian Zwierzchowski',
       author_email='sebastian.zwierzchowski@gmail.com',
       license='GPL2',
-      package_dir={'house': 'src/house'},
+      package_dir={'house': 'src'},
       packages=['house'],
       data_files=[('/etc/smarthouse', confFile),
-                  ('/var/www/smarthouse', 'src/SmartHouse.py'),
+                  ('/var/www/smarthouse', ['src/SmartHouse.py']),
                   ('/var/www/smarthouse/static', wwwStatic),
+                  ('/var/www/smarthouse/static/rfpilot', wwwStaticRf),
+                  ('/var/www/smarthouse/static/tvpilot', wwwStaticTv),
                   ('/var/www/smarthouse/templates', wwwTemp),
-                  ('/usr/lib/systemd/system', 'housed.service')],
+                  ('/usr/lib/systemd/system', ['housed.service'])],
       scripts=['src/housed.py'],
       requires=["flask", "pyserial"]
     )
