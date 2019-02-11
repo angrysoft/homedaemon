294034bb63565a1aac1b99873fa675e993ef6e21
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2019-02-11 21:48:00 +0100

    key

--- lib/house/aquara.py
+++ lib/house/aquara.py
@@ -3,9 +3,29 @@ import json
 import binascii
 from Crypto.Cipher import AES
 
+class Device:
+    _model = ''
+    _sid = ''
+
+    @property
+    def model(self):
+        return self._model
+
+    @model.setter
+    def model(self, value):
+        self._model = value
+
+    @property
+    def sid(self):
+        return self._sid
+
+    @sid.setter
+    def sid(self, value):
+        self._sid = value
+
 
 class Gateway:
-    def __init__(self, ip='auto', sid='', gwpasswd=''):
+    def __init__(self, ip='auto', sid='', gwpasswd='', token=''):
         self.aes_key_iv = bytes([0x17, 0x99, 0x6d, 0x09, 0x3d, 0x28, 0xdd, 0xb3, 0xba, 0x69, 0x5a, 0x2e, 0x6f, 0x58, 0x56, 0x2e])
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
         self.multicast = ('224.0.0.50', 4321)
@@ -16,7 +36,16 @@ class Gateway:
         else:
             self.unicast = (ip, 9898)
             self.sid = sid
-        self.gwpassword = gwpasswd
+        self.gwpasswd = gwpasswd
+        self._token = token
+
+    @property
+    def token(self):
+        return self._token
+
+    @token.setter
+    def token(self, value):
+        self._token = value
 
     def whois(self):
         """Discover the gateway device multicast (IP: 224.0.0.50 peer_port: 4321 protocal: UDP)"""
@@ -40,10 +69,16 @@ class Gateway:
 
         return self._send_unicast(cmd='read', sid=sid)
 
-    def write_device(self, _model, _sid, _params):
+    def write_device(self, model, sid, short_id, data):
         """Send the "write" command via unicast to the gateway's UDP 9898 port.
         When users need to control the device, the user can use the "write" command."""
-        return self._send_unicast(cmd='write', model=_model, sid=_sid, params=_params, key=self.get_key())
+        _data = data
+        _data['key'] = self.get_key().decode()
+        return self._send_unicast(cmd='write',
+                                  model=model,
+                                  sid=sid,
+                                  short_id=short_id,
+                                  data=data)
 
     def read_all_devices(self):
         id_list = self.get_id_list()
@@ -69,16 +104,17 @@ class Gateway:
         return self._answer()
 
     def _send_unicast(self, **kwargs):
-        self.sock.sendto(self._cmd(kwargs).encode(), self.unicast)
+        self.sock.sendto(self._cmd(kwargs).encode('ascii'), self.unicast)
         return self._answer()
 
-    def get_token(self):
-        return self.read_device(self.sid)
+    def refresh_token(self):
+        self._token = self.get_id_list().get('token')
 
-    def get_key(self, gwpass):
+    def get_key(self):
         """Get current gateway key"""
-        cipher = AES.new(gwpass, AES.MODE_CBC, self.aes_key_iv)
-        encrypted = cipher.encrypt(self.get_token())
+        cipher = AES.new(self.gwpasswd, AES.MODE_CBC, self.aes_key_iv)
+        self.refresh_token()
+        encrypted = cipher.encrypt(self.token)
         return binascii.hexlify(encrypted)
 
     def __del__(self):
@@ -90,5 +126,11 @@ if __name__ == '__main__':
     print(gw.whois())
     # print(gw.get_id_list())
     print(gw.get_device_list())
-    # print(gw.read_all_devices())
-    print(gw.get_token())
+    print(gw.read_all_devices())
+    gw.refresh_token()
+    devs = gw.read_all_devices()
+    print(gw.write_device(model="ctrl_neutral2",
+                          sid="158d00029b1929",
+                          short_id=44507,
+                          data={"channel_1": "off"}))
+    print(gw.token)
