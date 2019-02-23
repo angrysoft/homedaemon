187914a3c92a9f82d8f9281386153b257f5e707f
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2019-02-23 11:23:20 +0100

    omg

---
+++ homed.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# homed.py
+# Copyright (C) 2014-2019  Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+__author__ = 'Sebastian Zwierzchowski'
+__copyright__ = 'Copyright 2014-2019 Sebastian Zwierzchowski'
+__license__ = 'GPL2'
+__version__ = '0.7'
+
+
+import asyncio
+import sys
+sys.path.append('/etc/smarthouse')
+
+
+class HomeDaemon:
+    def __init__(self):
+        self.loop = asyncio.get_event_loop()
+
+    def event_watcher(self, ev):
+        """This method is """
+        print(ev)
