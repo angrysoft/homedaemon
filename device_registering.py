4a1a4cda947acb3f76b4c1a534b697731f04836b
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2019-09-13 22:30:30 +0200

    pią, 13 wrz 2019, 22:30:30 CEST

--- device_registering.py
+++ device_registering.py
@@ -109,7 +109,7 @@ class Register:
             d['name'] = self.names.get(d.get('sid'), '....')
             if d['name'] == 'Hall' and d['model'] == 'sensor_motion.aq2':
                 d['on_motion'] = 'hall_motion'
-                d['on_nomotion'] = {'120','hall_no_motion'}
+                d['on_nomotion'] = {'120':'hall_no_motion'}
             elif d['name'] == 'Kitchen Switch' and d['model'] == 'sensor_switch.aq2':
                 d['on_click'] = 'led_strip'
             print(f"\t {d.get('model')}  {d.get('sid')} {d.get('name')}")
