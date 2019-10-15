2acc190beda1c65087b049acdc5493f9b78d99ed
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2019-10-15 11:30:31 +0200

    wto, 15 paź 2019, 11:30:31 CEST

---
+++ homedaemon/inputs/timer.py
@@ -0,0 +1,16 @@
+from homedaemon.inputs import BaseInput
+import asyncio
+from datetime import datetime
+
+
+class Input(BaseInput):
+    def __init__(self, queue, config):
+        super(Input, self).__init__(queue)
+        self.name = 'timer'
+        self.loop.create_task(self.timer())
+
+    async def timer(self):
+        while True:
+            date = datetime.now()
+            self.queue.put({'cmd': 'report', 'sid': 'timer',  'data': {'time': f'{date.hour}:{date.minute}'}})
+            await asyncio.sleep(60)
