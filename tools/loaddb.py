c1f4a27d631c0c391659bb09837fbf87fe449801
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2019-12-23 13:39:02 +0100

    pon, 23 gru 2019, 13:39:02 CET

--- tools/loaddb.py
+++ tools/loaddb.py
@@ -2,17 +2,25 @@
 
 from pycouchdb import Server
 import json
+import argparse
 
-srv = Server()
 
-with open('dump.json', 'r') as jfile:
-    dbs = json.load(jfile)
-    for dbname in dbs:
+
+def load(dbname):
+    srv = Server()
+    with open(f'{dbname}.json', 'r') as jfile:
+        dbdata = json.load(jfile)
         if dbname in srv:
             srv.delete(dbname)
         srv.create(dbname)
         db = srv.db(dbname)
         print(f'Restoring {dbname}')
-        for doc in dbs[dbname]:
+        for doc in dbdata[dbname]:
             db.add(doc)
-    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(usage='%(prog)s [options] [packagees]')
+    parser.add_argument('dbs', nargs='*')
+    args = parser.parse_args()
+    for dbname in args.dbs:
+        load(dbname)
