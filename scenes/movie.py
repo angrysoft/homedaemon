2e924a0497bb3e43eaca6b5bc68fdd7e63726030
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2019-12-29 21:31:29 +0100

    nie, 29 gru 2019, 21:31:29 CET

---
+++ scenes/movie.py
@@ -0,0 +1,27 @@
+from homedaemon.scenes import BaseScene
+
+class Scene(BaseScene):
+    def __init__(self, daemon):
+        super().__init__(daemon)
+        self.name = 'Movie'
+        self.automatic = False
+    
+    def on(self):
+        dev_to_off = ['158d00027d0065', '158d00029b1929', '158d0002bffe5a']
+        
+        tvrgb = self.get_device('rgb01')
+        color = self.get_device('0x0000000007e7bae0')
+        tvrgb.on()
+        tvrgb.set_rgb({'red':0 ,'green':0, 'blue': 255})
+        tvrgb.bright(80)
+        color.on()
+        color.bright(50)
+        color.set_rgb({'red':0 ,'green':0, 'blue': 255})
+        for _sid in dev_to_off:
+            dev = self.get_device(_sid)
+            if dev.model == 'plug':
+                dev.power.off()
+            elif dev.model == 'ctrl_neutral1':
+                dev.channel_0.off()
+            elif dev.model in ('bslamp1', 'color', 'rgbstrip', 'bravia', 'ctrl_neutral2'):
+                dev.off()
