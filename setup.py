74e17bd4680ef49d4e694686c6c310972c52208d
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2019-12-30 22:08:16 +0100

    pon, 30 gru 2019, 22:08:16 CET

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_files(name):
             continue
 
 
-scenes = glob('files/scenes/*.py')
+scenes = glob('scenes/*.py')
 
 setup(
     name='AngryHome',
@@ -23,7 +23,7 @@ setup(
     author_email='sebastian.zwierzchowski@gmail.com',
     description='',
     scripts=['homed.py'],
-    requires=["flask", "pyserial", "pyxiaomi", "redis", 'pycouchdb'],
+    requires=["flask", "pyserial", "pyxiaomi", 'pycouchdb'],
     data_files=[('/etc/angryhome/scenes', scenes),
                 ('/usr/lib/systemd/system', ['homed.service'])],
 )
