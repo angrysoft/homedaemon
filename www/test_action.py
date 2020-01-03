bd7d734780f4d4d9592e92fd5f4c82d662b255a9
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2020-01-03 17:24:27 +0100

    pią, 3 sty 2020, 17:24:27 CET

---
+++ www/test_action.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+import json
+from gasistant import get_devices_list
+from pprint import pprint
+
+a_exec = '''{
+  "requestId": "ff36a3cc-ec34-11e6-b1a0-64510650abcf",
+  "inputs": [{
+    "intent": "action.devices.EXECUTE",
+    "payload": {
+      "commands": [{
+        "devices": [{
+          "id": "158d000283b219",
+          "customData": {
+            "fooValue": 74,
+            "barValue": true,
+            "bazValue": "sheepdip"
+          }
+        },{
+          "id": "456",
+          "customData": {
+            "fooValue": 36,
+            "barValue": false,
+            "bazValue": "moarsheep"
+          }
+        }],
+        "execution": [{
+          "command": "action.devices.commands.OnOff",
+          "params": {
+            "on": true
+          }
+        }]
+      }]
+    }
+  }]
+}
+'''
+a_query = '''
+{
+  "requestId": "ff36a3cc-ec34-11e6-b1a0-64510650abcf",
+  "inputs": [{
+    "intent": "action.devices.QUERY",
+    "payload": {
+      "devices": [{
+        "id": "123",
+        "customData": {
+          "fooValue": 74,
+          "barValue": true,
+          "bazValue": "foo"
+        }
+      },{
+        "id": "456",
+        "customData": {
+          "fooValue": 12,
+          "barValue": false,
+          "bazValue": "bar"
+        }
+      }]
+    }
+  }]
+}
+'''
+
+a_sync = '''{
+  "requestId": "ff36a3cc-ec34-11e6-b1a0-64510650abcf",
+  "inputs": [{
+    "intent": "action.devices.SYNC"
+  }]
+}'''
+
+from gasistant import Actions
+
+gs = Actions(a_sync)
+print(gs.execute())
+pprint(gs.response)
