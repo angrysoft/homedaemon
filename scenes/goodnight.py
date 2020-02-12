73bd311fdbc2c83a08d54c75c3d5353227705a45
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2020-02-12 23:02:36 +0100

    śro, 12 lut 2020, 23:02:36 CET

--- scenes/goodnight.py
+++ scenes/goodnight.py
@@ -9,7 +9,7 @@ class Scene(BaseScene):
     def on(self):
         dev_to_off = ['158d00024e2e5b', '158d00027d0065', '158d000283b219',
                       '158d00029b1929', '158d0002a16338', '158d0002a18c2b', '158d0002abac97',
-                      '158d0002bffe5a', 'rgb01', 'tv01']
+                      '158d0002bffe5a', 'rgb01', 'tv01', '0x0000000007200259']
         bslamp = self.get_device('0x000000000545b741')
         color = self.get_device('0x0000000007e7bae0')
         bslamp.on()
@@ -22,7 +22,7 @@ class Scene(BaseScene):
                 dev.power.off()
             elif dev.model == 'ctrl_neutral1':
                 dev.channel_0.off()
-            elif dev.model in ('bslamp1', 'color', 'rgbstrip', 'bravia', 'ctrl_neutral2'):
+            elif dev.model in ('bslamp1','desklamp', 'color', 'rgbstrip', 'bravia', 'ctrl_neutral2'):
                 dev.off()
         self.sleep(20)
         color.off()
