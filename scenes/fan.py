b15716601f9d58a326907abfb936e38f1de1b54c
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2020-03-08 10:31:53 +0100

    nie, 8 mar 2020, 10:31:53 CET

--- scenes/fan.py
+++ scenes/fan.py
@@ -1,5 +1,6 @@
 from homedaemon.scenes import BaseScene
 from datetime import datetime
+from threading import Timer
 
 class Scene(BaseScene):
     def __init__(self, daemon):
@@ -7,13 +8,22 @@ class Scene(BaseScene):
         self.name = 'bathrom fan'
         self.triggers = '158d0002abac97.channel_1.on'
         self.triggers = '158d0002abac97.channel_1.off'
-    
+        self.timer_fan_on = None
+        
     def on(self):
         wallsw = self.get_device('158d0002abac97')
         if wallsw.channel_1.is_on():
-            self.sleep(240)
-            if self.running and wallsw.channel_1.is_on():
-                wallsw.channel_0.on()
+            self.timer_fan_on(10, self.fan_on, wallsw)
+                
         elif wallsw.channel_1.is_off():
-            self.sleep(15)
-            wallsw.channel_0.off()
+            if self.timer_fan_on:
+                self.timer_fan_on.cancel()
+            self.sleep(5)
+            if wallsw.channel_1.is_off():
+                wallsw.channel_0.off()
+    
+    def fan_on(self, wallsw):
+        if self.running and wallsw.channel_1.is_on():
+                wallsw.channel_0.on()
+                self.timer_fan_on = None
+                
