e8da637c1c2be3502abb4d4a73ba3c3932062d1b
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2020-03-25 18:47:52 +0100

    śro, 25 mar 2020, 18:47:52 CET

--- homedaemon/devices/tv.py
+++ homedaemon/devices/tv.py
@@ -4,6 +4,7 @@ from pytvremote import Bravia
 class TvDevice:
     def __new__(cls, data, daemon):
         dev = {'KDL-48W585B': Bravia}.get(data.get('model'), Dummy)(data.get('ip'), data.get('mac'))
+        daemon.bus.on('write', dev.sid, dev.write)
         return dev
 
 
