0ec726e1110088a680cc38ead5c1302b44db8ffd
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2020-04-05 01:27:19 +0200

    nie, 5 kwi 2020, 01:27:19 CEST

--- scenes/ct_lamp.py
+++ scenes/ct_lamp.py
@@ -8,19 +8,27 @@ class Scene(BaseAutomation):
         self.add_trigger('report.clock.time.sunset', self.on_time)
         self.place = 'Living room'
         self.lamp = self.get_device('0x0000000007e7bae0')
-        self.clock = self.get_device('clock')
+    
+    def calc_ct(self, ct, delta, ts):
+        if delta <= 0:
+            return 0
+        return int(ct / (delta / ts))
     
     def on_time(self):
+        _ts = 600
         _sunset = datetime.now()
         _target_time = datetime(_sunset.year, _sunset.month, _sunset.day, 23, 0, 0)
-        _delta - _target_time - _sunset
+        _delta = _target_time - _sunset
         _delta = _delta.seconds
-        while _delta >= 0:
-            if lamp.is_on():
-                lamp.ct_pc(50)
-            _delta -= 600
-            print(_delta)
-            self.sleep(600)
+        _ct = 50
+        
+        while _delta >= 1:
+            _delta -= _ts
+            _ct -= self.calc_ct(_ct, _delta, _ts)
+            if _ct < 0:
+                break
+            self.lamp.ct_pc(_ct)
+            self.sleep(_ts)
         self.lamp.ct_pc(0)
         
             
