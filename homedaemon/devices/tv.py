172bee8b1b34c14084abb9b563b6d149f86252e4
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2020-07-04 22:04:24 +0200

    sob, 4 lip 2020, 22:04:24 CEST

--- homedaemon/devices/tv.py
+++ homedaemon/devices/tv.py
@@ -4,7 +4,6 @@ from pyiot.sony import Bravia
 class Driver:
     def __new__(cls, model, sid, config, daemon):
         dev = {'KDL-48W585B': Bravia}.get(model, Dummy)(config.get('ip'),
-                                                        config.get('mac'),
                                                         sid=sid,
                                                         model=model)
         daemon.bus.add_trigger(f'write.{dev.sid}.*.*', dev.write)
