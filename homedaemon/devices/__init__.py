c66a144e3bd61d0b2be79c4a61fc44b75bc15644
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2020-11-09 23:03:26 +0100

    pon, 9 lis 2020, 23:03:26 CET

--- homedaemon/devices/__init__.py
+++ homedaemon/devices/__init__.py
@@ -36,29 +36,32 @@ class Devices:
     def __new__(cls):
         if Devices._instance is None:
             cls.drivers: Drivers = Drivers()
-            cls._devices: Dict[str, Drivers] = dict()
-            cls._devices_fail_list: List[Dict[str,str]] = list()
+            cls._devices: Dict[str, Drivers] = {}
+            cls._devices_info_list: Dict[str,Dict[str,Any]] = {}
             cls.config = Config()
             cls.logger = Logger()
             Devices._instance = object.__new__(cls)
         return cls._instance
     
     def register_devices(self) -> None:
-        for dev in self.get_devices_info_list():
-            self.logger.debug(f"Loading....{dev['sid']} : {dev.get('name')} from {dev.get('place')}")
-            self.register_dev(dev)
+        self.load_devices_info()
+        while self._devices_info_list:
+            sid, device_info = self._devices_info_list.popitem()
+            self.logger.debug(f"Loading....{sid} : {device_info.get('name')} from {device_info.get('place')}")
+            self.register_dev(sid, device_info)
         
-        print(self.drivers.drivers_modules)
+            print(len(self._devices_info_list))
 
         
-    def get_devices_info_list(self) -> Generator[Dict[str,Any], None, None]:
+    def load_devices_info(self) -> None:
         try:
             _dir: Iterator[DirEntry[str]] = scandir(path=self.config.get('devices_dir'))
             for _file in _dir:
                 if _file.is_file and _file.name.endswith('.json'):
                     with open(_file.path) as dev_file:
-                        # ret.append(json.load(dev_file))
-                        yield json.load(dev_file)
+                        device_info = json.load(dev_file)
+                        if self._check_device_info(device_info):
+                            self._devices_info_list[device_info['sid']] = device_info
                         
                         
         except FileNotFoundError as fnf:
@@ -66,25 +69,32 @@ class Devices:
         except NotADirectoryError as nd:
             self.logger.error(f'Get devices list {nd}')
         
-    def register_dev(self, device_info: Dict[str, Any]):
-        if self._check_device_info(device_info):    
+    def register_dev(self, sid:str, device_info: Dict[str, Any]):
+            
             driver_info: Dict[str,str] = device_info['driver']
             if not self.drivers.is_module_loaded(driver_info['module']):
                 self.drivers.load_driver_module(driver_info['module'])
             driver = self.drivers.get_driver(driver_info['module'], driver_info['class'])
             if 'gateway' in driver_info:
-                gateway  = self._get_gateway(driver_info['gateway'])
-                # add to args 
+                if driver_info['gateway'] not in self._devices:
+                    _gateway_info = self._devices_info_list.pop(driver_info['gateway'])
+                    self.register_dev(driver_info['gateway'], _gateway_info)
+                device_info['args'] = self._devices[driver_info['gateway']]
+            
+            device_info['args']['sid'] = sid
+            self._devices[sid] = driver(**device_info['args'])
             
             # init dev with args
             # set dev name , place 
+            print(device_info)
     
     def _check_device_info(self, device_info: Dict[str,str]) -> bool:
         # TODO: check value is not empty
         return {'sid', 'driver', 'name', 'place'}.issubset(device_info)
     
     def _get_gateway(self, sid:str):
-        pass
+        gateway_config = self.config.get(sid)
+        
     
     def get(self, key, ret=None):
         try:
@@ -92,7 +102,8 @@ class Devices:
         except KeyError:
             return ret
     
-    # def get_devices_info_list(self) -> List[Dict[str,str]]:
+    def get_devices_info_list(self) -> List[Dict[str,str]]:
+        return []
     #     ret: List[Dict[str,str]] = list()
     #     for devitem in self._devices:
     #         dev = self.get(devitem)
