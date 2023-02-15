53779abe46c3d74d13f391058109a8e61c9f6110
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2023-02-15 23:18:08 +0100

    status

--- status/status.go
+++ status/status.go
@@ -4,29 +4,42 @@ import (
 	"errors"
 )
 
-type Status struct {
-	attributes map[string]any
+type Status[T any] struct {
+	attributes map[string]T
+	aliases    map[string]string
 }
 
-func CreateStatus() *Status {
-	return &Status{
+func CreateStatus() *Status[any] {
+	return &Status[any]{
 		attributes: make(map[string]any),
+		aliases:    make(map[string]string),
 	}
 }
 
-func (s *Status) Len() int {
+func (s *Status[_]) Len() int {
 	return len(s.attributes)
 }
 
-func (s *Status) Get(name string) any {
-	attr, ok := s.attributes[name]
+func (s *Status[T]) Get(name string) T {
+	result, ok := s.attributes[name]
 	if !ok {
-		return nil
+		attrName, ok := s.aliases[name]
+		if ok {
+			result = s.attributes[attrName]
+		}
 	}
-	return attr
+	return result
 }
 
-func (s *Status) RegisterAttribute(name string, attr any) error {
+func (s *Status[T]) Set(name string, value T) {
+	if _, ok := s.attributes[name]; ok {
+		s.attributes[name] = value
+	} else if attrName, ok := s.aliases[name]; ok {
+		s.attributes[attrName] = value
+	}
+}
+
+func (s *Status[T]) RegisterAttribute(name string, attr T) error {
 
 	if _, ok := s.attributes[name]; ok {
 		return errors.New("Attribute already registered")
@@ -35,14 +48,16 @@ func (s *Status) RegisterAttribute(name string, attr any) error {
 	return nil
 }
 
-func (s *Status) UnregisterAttribute(name string) {
+func (s *Status[_]) UnregisterAttribute(name string) {
 	delete(s.attributes, name)
 }
 
-// func (s *Status) AddAlias(aliasName, attrName string) error {
-// 	_, ok := s.attributes[attrName]
-// 	if ok {
-// 		s.attributes[aliasName] = s.attributes[attrName]
-// 	}
-// 	return nil
-// }
+func (s *Status[_]) AddAlias(aliasName, attrName string) error {
+	if _, ok := s.attributes[attrName]; ok {
+		if _, ok := s.aliases[aliasName]; ok {
+			return errors.New("Attribute already registered")
+		}
+		s.aliases[aliasName] = attrName
+	}
+	return nil
+}
