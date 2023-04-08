5073a865e1db2e1a40b87f222d8df67fb0437e0c
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2023-04-08 21:11:23 +0200

    ...

--- src/main/java/ovh/angrysoft/homedaemon/automation/conditions/IntGtTestCase.java
+++ src/main/java/ovh/angrysoft/homedaemon/automation/conditions/IntGtTestCase.java
@@ -1,16 +1,15 @@
 package ovh.angrysoft.homedaemon.automation.conditions;
 
-public class IntGtTestCase extends TestCase<Long> {
+public class IntGtTestCase extends TestCase<Integer> {
 
-    public IntGtTestCase(String type, String deviceSid, String attrName, Object attrValue) {
-        super(type, deviceSid, attrName, (Long) attrValue);
+    public IntGtTestCase(String type, String deviceSid, String attrName, Integer attrValue) {
+        super(type, deviceSid, attrName, attrValue);
     }
 
     @Override
     public boolean check(Object value) {
         if (value == null)
             return false;
-        return  (Long) value > attrValue;
+        return (Integer) value > attrValue;
     }
-
 }
