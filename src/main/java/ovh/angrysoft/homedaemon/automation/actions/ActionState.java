136114e4aa3f936941e0c015eb0662a419b167ef
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2023-04-27 22:35:46 +0200

    action state

--- src/main/java/ovh/angrysoft/homedaemon/automation/actions/ActionState.java
+++ src/main/java/ovh/angrysoft/homedaemon/automation/actions/ActionState.java
@@ -1,22 +1,21 @@
 package ovh.angrysoft.homedaemon.automation.actions;
 
-import ovh.angrysoft.homedaemon.bus.EventBus;
 import ovh.angrysoft.homedaemon.bus.Events.StatusEvent;
+import ovh.angrysoft.homedaemon.devices.DeviceManager;
 
 public class ActionState extends Action {
-    private EventBus bus;
+    private DeviceManager deviceManager;
     private String attrName;
     private Object attrValue;
 
-    public ActionState(String attrName, Object attrValue, EventBus bus) {
-        this.bus = bus;
+    public ActionState(String attrName, Object attrValue, DeviceManager deviceManager) {
         this.attrName = attrName;
         this.attrValue = attrValue;
+        this.deviceManager = deviceManager;
     }
 
     @Override
     public void run() {
-        //TODO this not update device state
-        bus.dispatch(new StatusEvent("state", attrName, attrValue));
+        deviceManager.update(new StatusEvent("state", attrName, attrValue));
     }
 }
