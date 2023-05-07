9590b0cb35ccc0fb780bbce12983bf15f3fd5cf8
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2023-05-07 22:13:01 +0200

    config

--- src/main/java/ovh/angrysoft/homedaemon/config/Config.java
+++ src/main/java/ovh/angrysoft/homedaemon/config/Config.java
@@ -1,51 +1,58 @@
 package ovh.angrysoft.homedaemon.config;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 public class Config {
+    private static final Logger LOGGER = Logger.getLogger("Homedaemon");
+
     private String configBaseDir;
-    private Map<String, Record> configsData;
-    private Map<String, Class<Record>> configsTypes;
+    private Map<String, ConfigType<?>> configs;
 
     public Config(String configBaseDir) {
         this.configBaseDir = configBaseDir;
-        this.configsData = new HashMap<>();
-        this.configsTypes = new HashMap<>();
+        this.configs = new HashMap<>();
     }
 
-    public void resisterConfigType(String name, Class<Record> configType) {
+    public <T> void resisterConfigType(String name, ConfigType<T> configType) {
         StringBuilder filePath = new StringBuilder(configBaseDir);
         filePath.append("/")
                 .append(name)
                 .append(".json");
-        this.configsTypes.put(name, new ConfigType());
-    }
-
-    public void loadConfig(String name) {
-        if (configsTypes.containsKey(name)) {
-            try {
-                Record conf = new Gson().fromJson(new FileReader(config), JsonObject.class);
-                String fileName = path.getFileName().toString();
-                String[] name = fileName.split(".json");
-                setCategory(name[0], conf);
-            } catch (FileNotFoundException e) {
-                LOGGER.log(Level.ALL, "Config file {0} not found", path.getFileName().toString());
-            } catch (JsonSyntaxException e) {
-                LOGGER.log(Level.ALL, "Config file parse error", path.getFileName().toString());
-            }
-
+        configType.setFilePath(filePath.toString());
+        try {
+            configType.setConfigData(new Gson().fromJson(new FileReader(filePath.toString()), configType.getConfigClass()));
+        } catch (FileNotFoundException e) {
+            LOGGER.log(Level.ALL, "Config file {0} not found", configType.getFilePath());
+        } catch (JsonSyntaxException e) {
+            LOGGER.log(Level.ALL, "Config file parse error", configType.getFilePath());
         }
+        this.configs.put(name, configType);
+    }
+
+    // public void loadConfig(String name) {
+    //     if (configs.containsKey(name)) {
+    //         ConfigType configT = configsTypes.get(name);
+    //         try {
+    //             configsData.put(name, new Gson().fromJson(new FileReader(configT.filePath()), configT.configType()));
+    //         } catch (FileNotFoundException e) {
+    //             LOGGER.log(Level.ALL, "Config file {0} not found", configT.filePath());
+    //         } catch (JsonSyntaxException e) {
+    //             LOGGER.log(Level.ALL, "Config file parse error", configT.filePath());
+    //         }
+    //     }
+    // }
+
+    public Record get(String configName) {
+        ConfigType<?>  conf = configs.get(configName);
+        return (Record) conf.getConfigData();
     }
 }
 
-class ConfigType {
-    private String filePath;
-    private Calss<Record> configType;
-
-    public ConfigType(file) {
-
-    }
-}
--- src/main/java/ovh/angrysoft/homedaemon/config/Homed.java
+++ src/main/java/ovh/angrysoft/homedaemon/config/Homed.java
@@ -3,3 +3,5 @@ package ovh.angrysoft.homedaemon.config;
 public record Homed(String name, String id, String[] io, boolean debug ) {
     
 }
+
+
