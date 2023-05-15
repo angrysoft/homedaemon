6d495daeec263b688ef99bc0cb2c0ef14961314f
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2023-05-15 21:40:34 +0200

    cleaning before merge

--- panel/static/admin/devices.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:html';
-
-import '../lib/modal.dart';
-
-void main() {
-  Modal devadd = new Modal.fromHtml('devadd');
-  ButtonElement addbtn = querySelector('#addbtn');
-  addbtn.onClick.listen((e)=> devadd.show()); 
-}
