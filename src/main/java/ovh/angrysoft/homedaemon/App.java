e6b55c5a34ff273b0f56718f42b0e95c2ca98864
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2023-05-26 19:36:26 +0200

    license

--- src/main/java/ovh/angrysoft/homedaemon/App.java
+++ src/main/java/ovh/angrysoft/homedaemon/App.java
@@ -1,5 +1,5 @@
 /*
-Copyright [2023] [Sebastian Zwierzchowski aka Angrysoft]
+Copyright 2023 Sebastian Zwierzchowski aka Angrysoft
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
