9f805cb7cec15320e9446b21e741384a2f909567
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2024-10-25 21:10:17 +0200

    spring-init

---
+++ src/main/java/ovh/angrysoft/homedaemon/HomedaemonApplication.java
@@ -0,0 +1,13 @@
+package ovh.angrysoft.homedaemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomedaemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomedaemonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ovh/angrysoft/homedaemon/HomedaemonApplicationTests.java
@@ -0,0 +1,13 @@
+package ovh.angrysoft.homedaemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomedaemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
